2349cef59b1240179b037d30bfcc63d3608c38e9
Victor Fuentes <21144@columbushs.com>
2019-12-09 10:24:06 -0500

    First Commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,34 @@
 // Soldier
-class Soldier {}
-
+class Soldier {
+   constructor(health, strength){ 
+    this.health = health;
+    this.strength = strength;
+}
+    attack(){
+        return this.strength
+    }
+    receiveDamage(theDamage){   
+        this.health -= theDamage
+    }
+}
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name,health, strength){
+        super(name,health,strength);
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+    receiveDamage(theDamage){
+        theDamage = 50
+        this.health -= theDamage
+        if (this.health > 0){
+        return (this.name + " has received" + theDamage + "points of damage");
+        }
+        else if (this.health < 0)
+        return (this.name 
+    }  
+}
 
 // Saxon
 class Saxon {}
